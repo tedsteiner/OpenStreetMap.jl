6b33069f8fc70169d0851c2e640018584aef6686
Ted Steiner <tsteiner2@gmail.com>
2014-06-17 11:31:21 -0400

    Added REQUIRE file. Moved source files into src folder.
    Signed-off-by: Ted Steiner <tsteiner2@gmail.com>

--- src/OpenStreetMap.jl
+++ src/OpenStreetMap.jl
@@ -1,5 +1,28 @@
+###################################
+### Julia OpenStreetMap Package ###
+### MIT License                 ###
+### Copyright 2014              ###
+###################################
+
 module OpenStreetMap
 
-# package code goes here
+import LightXML
+import Winston
 
-end # module
+export parseMapXML
+export getNodes, getBounds, getHighways, getBuildings, getFeatures
+export plotMap, cropMap!, findIntersections
+
+include("types.jl")
+include("parseMap.jl")
+include("nodes.jl")
+include("bounds.jl")
+include("highways.jl")
+include("features.jl")
+include("buildings.jl")
+
+include("crop.jl")
+include("plot.jl")
+include("intersections.jl")
+
+end
