41f8c47768ac8ec798adbc4947d5cb5a45674857
Ted Steiner <tsteiner2@gmail.com>
2014-07-17 14:47:49 -0400

    Changed coordinates.jl unit test to use approximate floating point comparison.
    Signed-off-by: Ted Steiner <tsteiner2@gmail.com>

--- tests/coordinates.jl
+++ tests/coordinates.jl
@@ -22,24 +22,24 @@ P_ref_lla = OpenStreetMap.LLA(lat0,lon0,alt0)
 # lla2ecef
 P_ecef = lla2ecef(P_lla)
 
-@test P_ecef.x == 1529073.1560519305
-@test P_ecef.y == -4465040.019013103
-@test P_ecef.z == 4275835.339260309
+@test_approx_eq P_ecef.x 1529073.1560519305
+@test_approx_eq P_ecef.y -4465040.019013103
+@test_approx_eq P_ecef.z 4275835.339260309
 
 # lla2enu
 P_enu = lla2enu(P_lla,P_ref_lla)
 
-@test P_enu.east == -343.49374908345493
-@test P_enu.north == 478.7648554687071
-@test P_enu.up == -0.027242884564486758
+@test_approx_eq P_enu.east -343.49374908345493
+@test_approx_eq P_enu.north 478.7648554687071
+@test_approx_eq P_enu.up -0.027242884564486758
 
 #####
 # Bounds object
 bounds = OpenStreetMap.Bounds(42.365,42.3695,-71.1,-71.094)
 bounds_enu = lla2enu(bounds)
 
-@test bounds_enu.min_lat == -249.92653559014204
-@test bounds_enu.max_lat == 249.9353534127322
-@test bounds_enu.min_lon == -247.1091823451915
-@test bounds_enu.max_lon == 247.1268196141778
+@test_approx_eq bounds_enu.min_lat -249.92653559014204
+@test_approx_eq bounds_enu.max_lat 249.9353534127322
+@test_approx_eq bounds_enu.min_lon -247.1091823451915
+@test_approx_eq bounds_enu.max_lon 247.1268196141778
 
