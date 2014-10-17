d5e35de5dfd4d42a3e53d0c3f340607de30caf81
yeesian <ngyeesian@gmail.com>
2014-10-17 10:56:52 -0400

    update tests for routes.jl

--- test/routes.jl
+++ test/routes.jl
@@ -96,82 +96,53 @@ loc0 = nodesENU[node0]
 filteredENU = filter((k,v)->haskey(network.v,k), nodesENU)
 local_indices = nodesWithinRange(filteredENU, loc0, 100.0)
 @test length(local_indices) == 4
-@test local_indices[1] == 61317384
-@test local_indices[2] == 23
-@test local_indices[3] == 61317383
-@test local_indices[4] == 17
+for index in [61317384, 23, 61317383, 17]
+    @test index in local_indices
+end
 
 # Nodes within driving distance
 start_index = nearestNode(filteredENU, loc0)
 node_indices, distances = nodesWithinDrivingDistance(network, start_index, 300.0)
 @test length(node_indices) == length(distances)
 @test length(node_indices) == 14
-@test node_indices[1] == 61318574
-@test node_indices[2] == 18
-@test node_indices[4] == 17
-@test node_indices[7] == 575472710
-@test node_indices[9] == 61317383
-@test node_indices[11] == 12
-@test node_indices[13] == 61318436
-@test_approx_eq_eps distances[1] 273.370463 1e-5
-@test_approx_eq_eps distances[3] 181.984201 1e-5
-@test_approx_eq_eps distances[5] 294.8091389 1e-5
-@test_approx_eq_eps distances[6] 124.141919 1e-5
-@test_approx_eq_eps distances[8] 49.373168 1e-5
-@test_approx_eq_eps distances[11] 237.6051207 1e-5
-@test_approx_eq_eps distances[13] 240.895672 1e-5
+for index in [61318574, 18, 17, 575472710, 61317383, 12, 61318436]
+    @test index in node_indices
+end
+for dist in distances
+    @test 0.0 <= dist <= 300.0
+end
 
 # Test nodes within driving distance, with multi-start
 node_indices, distances = nodesWithinDrivingDistance(network, local_indices, 300.0)
 @test length(node_indices) == length(distances)
 @test length(node_indices) == 29
-@test node_indices[1] ==   61318572
-@test node_indices[5] == 270134897
-@test node_indices[10] == 61318575
-@test node_indices[15] == 61317384
-@test node_indices[20] == 270134937
-@test node_indices[25] == 61318436
-@test node_indices[29] == 61332101
-@test_approx_eq_eps distances[1] 293.46982726054546 1e-5
-@test_approx_eq_eps distances[5] 296.07515014727875 1e-5
-@test_approx_eq_eps distances[10] 239.86445624635402 1e-5
-@test_approx_eq_eps distances[15] 0.0 1e-5
-@test_approx_eq_eps distances[20] 286.64003818285556 1e-5
-@test_approx_eq_eps distances[25] 185.95098954713148 1e-5
-@test_approx_eq_eps distances[29] 299.0071494358331 1e-5
+for index in [61318572, 270134897, 61318575, 61317384, 270134937, 61318436, 61332101]
+    @test index in node_indices
+end
+for dist in distances
+    @test 0.0 <= dist <= 300.0
+end
 
 # Nodes within driving time
 node_indices, distances = nodesWithinDrivingTime(network, start_index, 50.0)
 @test length(node_indices) == length(distances)
 @test length(node_indices) == 31
-@test node_indices[1] == 61318572
-@test node_indices[5] == 33
-@test node_indices[10] == 270134895
-@test node_indices[15] == 575440057
-@test node_indices[20] == 61323886
-@test node_indices[25] == 473951349
-@test node_indices[30] == 986189343
-@test_approx_eq_eps distances[1] 45.544798726849216 1e-5
-@test_approx_eq_eps distances[5] 48.04166713005169 1e-5
-@test_approx_eq_eps distances[10] 44.62973344050917 1e-5
-@test_approx_eq_eps distances[15] 44.29039553503941 1e-5
-@test_approx_eq_eps distances[20] 33.10080430980987 1e-5
-@test_approx_eq_eps distances[25] 45.06680630696164 1e-5
-@test_approx_eq_eps distances[30] 46.51756575933207 1e-5
+for index in [61318572, 33, 270134895, 575440057, 61323886, 473951349, 986189343]
+    @test index in node_indices
+end
+for dist in distances
+    @test 0.0 <= dist <= 50.0
+end
 
 # Test nodes within driving time, with multi-start
 node_indices, distances = nodesWithinDrivingTime(network, local_indices, 50.0)
 @test length(node_indices) == length(distances)
 @test length(node_indices) == 42
-@test node_indices[1] == 575444707
-@test node_indices[10] == 270134899
-@test node_indices[20] == 30
-@test node_indices[30] == 270134936
-@test node_indices[40] == 986189343
-@test_approx_eq_eps distances[1] 47.89297866535772 1e-5
-@test_approx_eq_eps distances[10] 37.96993889800794 1e-5
-@test_approx_eq_eps distances[20] 37.96993889800794 1e-5
-@test_approx_eq_eps distances[30] 49.33824153819995 1e-5
-@test_approx_eq_eps distances[40] 37.52661768605188 1e-5
+for index in [575444707, 270134899, 30, 270134936, 986189343]
+    @test index in node_indices
+end
+for dist in distances
+    @test 0.0 <= dist <= 50.0
+end
 
 end # module TestRoutes
